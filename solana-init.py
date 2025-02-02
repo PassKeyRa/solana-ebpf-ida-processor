cde53de41cf30cad79f3d7b5c098b763c8720d47
Alex <posikera@bk.ru>
2025-02-02 12:02:05 +0800

    upd

--- solana-init.py
+++ solana-init.py
@@ -5,7 +5,8 @@ from idaapi import *
 
 # Add the path to the IDA procs directory to the sys.path
 ida_procs_dir = pathlib.Path(__file__).parent.parent / 'procs'
-sys.path.append(str(ida_procs_dir))
+if ida_procs_dir not in sys.path:
+    sys.path.append(str(ida_procs_dir))
 
 from solana.processor import EBPFProc
 
